b277275d593cb971c1ca862ebff3e012033e28ca
kadeembestteaches <56241232+kadeembestteaches@users.noreply.github.com>
2020-12-17 12:55:35 -0500

    Allow Access to all

--- server.js
+++ server.js
@@ -27,7 +27,12 @@ const options = {
   
   const swaggerSpecification = swagerJsDoc(options);
 
-
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+  
 //cors middleware
 app.use(cors());
 
